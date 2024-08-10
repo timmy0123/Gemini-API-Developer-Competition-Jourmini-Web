c226b7067f2de6bbdbd1ca7596b7e58e90176d04
鄭宇伸 <timmyabc10@gmail.com>
2024-08-10 12:55:13 +0800

    Modify UI error

--- src/components/suggestion/SuggestionBox.vue
+++ src/components/suggestion/SuggestionBox.vue
@@ -153,6 +153,8 @@ const handleHover = (coordinates) => {
 }
 
 .recommand-location-title {
+  display: inline-block;
+  width: 100%;
   margin-top: 2rem;
   padding-top: 1rem;
   border-top: 2px solid #fff;
