7fd74149eba2c017a5a8573c61c540abd3a71c9f
Mark Chen <31685881+yuting1214@users.noreply.github.com>
2024-08-10 21:06:14 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  devServer: {
+    // Use environment variable PORT, or default to 3000 if not set
+    port: process.env.PORT || 3000, 
+    // Public URL to be used in development
+    public: process.env.RAILWAY_PUBLIC_DOMAIN || "http://localhost:8080", // Default to localhost if not set
+    host: "0.0.0.0", // Allow access from any network interface
+  },
 });
